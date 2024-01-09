7666b855bdeb838fb3b07a54a1685af78e7ad0ff
Premkumar0000 <premthadi25@gmail.com>
2024-01-09 14:22:41 +0530

    first

--- login.js
+++ login.js
@@ -1,5 +1,12 @@
 function submitForm() {
- var emailValue = document.getElementById('m').value;
- var passwordValue = document.getElementById('n').value;
-  alert("Form submitted!");
+  var emailValue = document.getElementById('m').value;
+  var passwordValue = document.getElementById('n').value;
+
+  if (emailValue === '') {
+    alert('Enter email');
+  }
+
+  if (passwordValue === '') {
+    alert('Enter password');
+  }
 }
